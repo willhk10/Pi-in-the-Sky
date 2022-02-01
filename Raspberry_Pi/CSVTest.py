0bbb4abba0d997af41f4ac84f63cedaad5d3e3cf
adent11 <adent11@charlottesvilleschools.org>
2022-02-01 18:33:35 +0000

    Add (nonfunctional) code to record sensor data to csv

--- Raspberry_Pi/CSVTest.py
+++ Raspberry_Pi/CSVTest.py
@@ -1,18 +1,24 @@
 import board
 import adafruit_mpl3115a2
-from datetime import datetime, timedelta
-
+from datetime import datetime, timedelta, timezone
+import csv
+import os
 
 i2c = board.I2C()
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60)
 sensor.sealevel_pressure = 102250
 
-startTime = datetime.now()
+est = timezone(timedelta(hours = -5)) # Creates est timezone 5:00 behind utc
+startTime = datetime.now(est)
 curTime = startTime
-nextPrintTime= curTime + timedelta(microseconds = .5)
-
-while curTime - startTime < timedelta(seconds = 5):
-    curTime =  datetime.now()
-    if curTime - nextPrintTime > timedelta(microseconds = 0):
-        print(f"Altitude: {sensor.altitude} \n Time: {curTime} \n")
-        nextPrintTime = curTime + timedelta(microseconds = .5)
+nextPrintTime= curTime + timedelta(seconds = .5)
+fpath = startTime.strftime("/%m-%d-%Y~%H:%M~barometerdata.csv")
+os.mkdir(fpath)
+with open(fpath) as f:
+    writer = csv.writer(f)
+    while curTime - startTime < timedelta(seconds = 5):
+        curTime =  datetime.now(est)
+        if curTime - nextPrintTime > timedelta(microseconds = 0):
+            print(f"Altitude: {sensor.altitude} \n Time: {curTime} \n")
+            writer.writerow([curTime, sensor.altitude])
+            nextPrintTime = curTime + timedelta(seconds = .5)
