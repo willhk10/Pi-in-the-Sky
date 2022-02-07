dca8637702b35d57789699e91fd5ab9e7778cf32
adent11 <adent11@charlottesvilleschools.org>
2022-02-07 18:15:27 +0000

    Record to csv after launch to save time

--- Raspberry_Pi/CSVRecorder.py
+++ Raspberry_Pi/CSVRecorder.py
@@ -19,6 +19,8 @@ GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 est = timezone(timedelta(hours = -5)) # Creates est timezone 5:00 behind utc
 
+flightData = []
+
 print("Waiting for button press")
 while GPIO.input(buttonPin): # Waits for a button press
     pass
@@ -26,21 +28,22 @@ while not GPIO.input(buttonPin): # Then a button release to start recording
     pass
 print("Recording")
 
-csvPath = datetime.now(est).strftime("%m-%d-%Y_%H:%M_barometerdata.csv")
+startTime = datetime.now(est)
+csvPath = startTime.strftime("%m-%d-%Y_%H:%M_barometerdata.csv")
+while GPIO.input(buttonPin): # Records until button pressed
+    curTime = datetime.now(est)
+    if True:
+        accel, mag = lsm303.read()
+        accelX, accelY, accelZ = accel
+        accelX = round(accelX/107, 3)
+        accelY = round(accelY/107, 3)
+        accelZ = round(accelZ/107, 3)
+        secFromStart = (curTime-startTime).total_seconds()
+        flightData.append([curTime, secFromStart, altimeter.altitude, altimeter.pressure, altimeter.temperature, accelX, accelY, accelZ])
+
 with open(csvPath, 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(['timestamp', 'seconds from start', 'altitude', 'pressure', 'temperature', 'accelerometer x', 'accelerometer y', 'accelerometer z']) # CSV title row
-    startTime = datetime.now(est)
-    nextTime = startTime + timedelta(microseconds = 500000)
-    while GPIO.input(buttonPin): # Records until button pressed
-        curTime = datetime.now(est)
-        if True: #curTime - nextTime > timedelta(microseconds = 0):
-            accel, mag = lsm303.read()
-            accelX, accelY, accelZ = accel
-            accelX = round(accelX/107, 3)
-            accelY = round(accelY/107, 3)
-            accelZ = round(accelZ/107, 3)
-            secFromStart = (curTime-startTime).total_seconds()
-            writer.writerow([curTime, secFromStart, altimeter.altitude, altimeter.pressure, altimeter.temperature, accelX, accelY, accelZ])
-            nextTime = nextTime + timedelta(microseconds = 500000)
+    writer.writerow(['timestamp', 'seconds from start', 'altitude', 'pressure', 'temperature', 'accelerometer x', 'accelerometer y', 'accelerometer z'])
+    writer.writerows(flightData)
+
 print("Recording Ended")
