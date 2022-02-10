12a6841835ec6ac8b95621ae93de6162f65b59a7
adent11 <adent11@charlottesvilleschools.org>
2022-02-10 17:49:41 +0000

    Fix led test and add servo test

--- Raspberry_Pi/LEDTest.py
+++ Raspberry_Pi/LEDTest.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 
-rPin, bPin, gPin = 25, 23, 18
+rPin, gPin, bPin = 25, 23, 18
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(rPin, GPIO.OUT)
@@ -10,4 +10,10 @@ GPIO.setup(bPin, GPIO.OUT)
 
 GPIO.output(rPin, True)
 GPIO.output(bPin, False)
-GPIO.output(gPin, False)
+GPIO.output(gPin, True)
+
+while True:
+    rgb = input()
+    GPIO.output(rPin, int(rgb[0]))
+    GPIO.output(gPin, int(rgb[1]))
+    GPIO.output(bPin, int(rgb[2]))
