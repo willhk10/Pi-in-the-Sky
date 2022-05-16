cdd542bcc883182d0971e727372f9947fe21a094
adent11 <adent11@charlottesvilleschools.org>
2022-05-16 18:14:49 +0100

    5/16 changes

--- Raspberry_Pi/CSVRecorder.py
+++ Raspberry_Pi/CSVRecorder.py
@@ -5,6 +5,7 @@ import RPi.GPIO as GPIO
 from datetime import datetime, timedelta, timezone
 import csv
 import os
+from time import sleep
 
 i2c = board.I2C()
 altimeter = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60)
@@ -41,43 +42,45 @@ def setLED(rgb):
   GPIO.output(gPin, int(rgb[1]))
   GPIO.output(bPin, int(rgb[2]))
 
-setLED(yellow)
-print("Waiting for button press")
-while GPIO.input(recordButtonPin): # Waits for a button press
-  if not GPIO.input(servoButtonPin):
-    if servoIsOpen:
-      servo.ChangeDutyCycle(closedCycle)
-      servoIsOpen = False
-    elif not servoIsOpen:
-      servo.ChangeDutyCycle(openCycle)
-      servoIsOpen = True
-    while not GPIO.input(servoButtonPin):
-      pass # Wait for button release
-while not GPIO.input(recordButtonPin): # Then a button release to start recording
-  pass
-print("Recording")
-setLED(green)
-flightData = []
-alt = altimeter.altitude
-maxAlt = alt
-startAlt = alt
-startTime = datetime.now(est)
-csvPath = startTime.strftime("%m-%d-%Y_%H:%M_barometerdata.csv")
-while GPIO.input(recordButtonPin): # Records until button pressed
-  curTime = datetime.now(est)
-  lastAlt = alt
+while True:
+  setLED(yellow)
+  print("Waiting for button press")
+  while GPIO.input(recordButtonPin): # Waits for a button press
+    if not GPIO.input(servoButtonPin):
+      if servoIsOpen:
+        servo.ChangeDutyCycle(closedCycle)
+        servoIsOpen = False
+      elif not servoIsOpen:
+        servo.ChangeDutyCycle(openCycle)
+        servoIsOpen = True
+      while not GPIO.input(servoButtonPin):
+        pass # Wait for button release
+  while not GPIO.input(recordButtonPin): # Then a button release to start recording
+    pass
+  print("Recording")
+  setLED(green)
+  flightData = []
   alt = altimeter.altitude
-  if alt > maxAlt:
-    maxAlt = alt
-  if alt + 2 < maxAlt and alt > startAlt + 1:
-    servo.ChangeDutyCycle(openCycle)
-    setLED(blue)
-  secFromStart = (curTime-startTime).total_seconds()
-  flightData.append([curTime, secFromStart, alt])
+  maxAlt = alt
+  startAlt = alt
+  startTime = datetime.now(est)
+  csvPath = startTime.strftime("/home/pi/Documents/Pi-in-the-Sky/Raspberry_Pi/CSVData/%m-%d-%Y_%H:%M_barometerdata.csv")
+  while GPIO.input(recordButtonPin): # Records until button pressed
+    curTime = datetime.now(est)
+    lastAlt = alt
+    alt = altimeter.altitude
+    if alt > maxAlt:
+      maxAlt = alt
+    if alt + 2 < maxAlt and alt > startAlt + 1:
+      servo.ChangeDutyCycle(openCycle)
+      setLED(blue)
+    secFromStart = (curTime-startTime).total_seconds()
+    flightData.append([curTime, secFromStart, alt])
 
-with open(csvPath, 'w') as f:
-  writer = csv.writer(f)
-  writer.writerow(['timestamp', 'seconds from start', 'altitude'])
-  writer.writerows(flightData)
-setLED(red)
-print("Recording Ended")
+  with open(csvPath, 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['timestamp', 'seconds from start', 'altitude'])
+    writer.writerows(flightData)
+  setLED(red)
+  print("Recording Ended")
+  sleep(.3)
